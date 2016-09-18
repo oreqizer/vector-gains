fc58b37c69b5acd6f058d1346554af3fe3e53c53
oreqizer <oreqizer@gmail.com>
2016-09-18 08:49:43 +0200

    Transform: Simplify matrix

--- Vectors/Transform.hs
+++ Vectors/Transform.hs
@@ -5,10 +5,12 @@ import Graphics.UI.GLUT
 import Vectors.Core
 import Utils.Angles
 
-type Matrix3 = (Vector3f, Vector3f, Vector3f)
+type MatrixRow3 = (GLfloat, GLfloat, GLfloat)
 
-prodMatrixRow :: Vector3f -> Vector3f -> GLfloat
-prodMatrixRow (Vector3 v1 v2 v3) (Vector3 m1 m2 m3) = v1 * m1 + v2 * m2 + v3 * m3
+type Matrix3 = (MatrixRow3, MatrixRow3, MatrixRow3)
+
+prodMatrixRow :: Vector3f -> MatrixRow3 -> GLfloat
+prodMatrixRow (Vector3 v1 v2 v3) (m1, m2, m3) = v1 * m1 + v2 * m2 + v3 * m3
 
 prodMatrix :: Matrix3 -> Vector3f -> Vector3f
 prodMatrix (m1, m2, m3) v = Vector3 r1 r2 r3
@@ -20,23 +22,23 @@ prodMatrix (m1, m2, m3) v = Vector3 r1 r2 r3
 xyRotMatrix :: GLfloat -> Matrix3
 xyRotMatrix d = (m1, m2, m3)
     where
-        m1 = Vector3 (cos $ rads d) (-(sin $ rads d)) 0
-        m2 = Vector3 (sin $ rads d) (cos $ rads d) 0
-        m3 = Vector3 0 0 1
+        m1 = ((cos $ rads d), (-(sin $ rads d)), 0)
+        m2 = ((sin $ rads d), (cos $ rads d), 0)
+        m3 = (0, 0, 1)
 
 xzRotMatrix :: GLfloat -> Matrix3
 xzRotMatrix d = (m1, m2, m3)
     where
-        m1 = Vector3 (cos $ rads d) 0 (sin $ rads d)
-        m2 = Vector3 0 1 0
-        m3 = Vector3 (-(sin $ rads d)) 0 (cos $ rads d)
+        m1 = ((cos $ rads d), 0, (sin $ rads d))
+        m2 = (0, 1, 0)
+        m3 = ((-(sin $ rads d)), 0, (cos $ rads d))
 
 yzRotMatrix :: GLfloat -> Matrix3
 yzRotMatrix d = (m1, m2, m3)
     where
-        m1 = Vector3 1 0 0
-        m2 = Vector3 0 (cos $ rads d) (-(sin $ rads d))
-        m3 = Vector3 0 (sin $ rads d) (cos $ rads d)
+        m1 = (1, 0, 0)
+        m2 = (0, (cos $ rads d), (-(sin $ rads d)))
+        m3 = (0, (sin $ rads d), (cos $ rads d))
 
 scalePoint :: GLfloat -> GLfloat -> GLfloat -> Vector3f -> Vector3f
 scalePoint xs ys zs (Vector3 x y z) = Vector3 (x * xs) (y * ys) (z * zs)
